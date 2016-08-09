aa80940ff46fd62fb947c4eff7f615e62b12fcf9
Matthew Tang <trailcode@Matthews-MacBook-Pro.local>
2016-08-08 20:56:14 -0600

    Experimenting with numpy

--- ServosPosGraph.py
+++ ServosPosGraph.py
@@ -5,6 +5,8 @@ from PyQt4.QtOpenGL import *
 from OpenGL.GL import *
 from PyQt4.QtGui import *
 from scipy.interpolate import interp1d
+from scipy.interpolate import splrep
+import numpy as np
 
 class ServosPosGraph(QGLWidget):
     def __init__(self, servoAdjustment, parent = None):
@@ -14,7 +16,7 @@ class ServosPosGraph(QGLWidget):
         self.setSizePolicy(sizePolicy)
         self.sliders = servoAdjustment.sliders
         self.servoAdjustment = servoAdjustment
-        self.colors = [(0,0,0),(255,255,255),(255,0,0),(0,255,0),(0,0,255),(255,255,0),(0,255,255),(255,0,255),
+        self.colors = [(70,128,50),(255,255,255),(255,0,0),(0,255,0),(0,0,255),(255,255,0),(0,255,255),(255,0,255),
                        (192,192,192),(128,128,128),(128,0,0),(128,128,0),(0,128,0),(128,0,128),(0,128,128),(0,0,128)]
 
     def paintGL(self):
@@ -48,14 +50,43 @@ class ServosPosGraph(QGLWidget):
 
             glColor3f(self.colors[j][0], self.colors[j][1], self.colors[j][2])
             glBegin(GL_LINE_STRIP)
-            for i in range(0,256,8):
-                keyPair = self.servoAdjustment.getCurrKeyPair(value = i)
+
+            r = list(range(0,256,16)) + list(ani.keys())
+            r.sort()
+            keys = list(ani.keys())
+            values = list(ani.values())
+            #l = ([(x, y) for (x, y) in sorted(zip(keys, values))])
+            for (x, y) in sorted(zip(keys, values)):
+                print('x,y',x,y)
+
+            #print('aaaa',l)
+            #l = sorted(zip(keys, values))
+
+            print('keys', keys)
+            for i in r:
+                keyPair = self.servoAdjustment.getCurrKeyPair(value=i)
                 if keyPair is None: continue
-                A = self.servoAdjustment.animation[keyPair[0]]
-                B = self.servoAdjustment.animation[keyPair[1]]
+                A = ani[keyPair[0]]
+                B = ani[keyPair[1]]
+                # print('keyPair[0]', keyPair[0], A[j])
+                #print('int',(keyPair[0], keyPair[1]), (A[j], B[j]))
+                intp = interp1d((keyPair[0], keyPair[1]), (A[j], B[j]))
+                #intp = splrep()
+                glVertex2d(i, intp(i))
+
+            """
+            r = list(ani.keys())
+            r.sort()
+            for i in r:
+                keyPair = self.servoAdjustment.getCurrKeyPair(value=i)
+                if keyPair is None: continue
+                A = ani[keyPair[0]]
+                B = ani[keyPair[1]]
                 intp = interp1d((keyPair[0], keyPair[1]), (A[j], B[j]))
                 glVertex2d(i, intp(i))
+            """
             glEnd()
+
 
     def resizeGL(self, w, h):
         glMatrixMode(GL_PROJECTION)
